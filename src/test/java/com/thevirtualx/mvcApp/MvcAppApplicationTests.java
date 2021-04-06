3415b3fa777617efc0688b2f49be54ecad4318b5
David Hooper <david@wecancodeit.org>
2021-04-06 10:10:40 -0600

    added folders

---
+++ src/test/java/com/thevirtualx/mvcApp/MvcAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thevirtualx.mvcApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
