02ca1fb89204fd3b982b62b1316bca24ad55d7de
Connor <connor@chamfam.net>
2021-04-12 13:33:35 -0400

    typo

--- src/main/java/com/thevirtualx/mvcApp/Controller/ChallengeController.java
+++ src/main/java/com/thevirtualx/mvcApp/Controller/ChallengeController.java
@@ -8,7 +8,7 @@ public class ChallengeController {
 
     private ChallengeStorage challengeStorage;
 
-    public ChatroomController(ChallengeStorage challengeStorage){
+    public ChallengeController (ChallengeStorage challengeStorage){
         this.challengeStorage = challengeStorage;
     }
 }
