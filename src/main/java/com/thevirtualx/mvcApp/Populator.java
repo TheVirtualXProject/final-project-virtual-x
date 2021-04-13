0486341587b83b413b157d0ebb9e37c1739dc6b9
Jelanie Starks <77803651+JelanieStarks@users.noreply.github.com>
2021-04-13 14:05:24 -0400

    added accounts for current members to populator, added 3 challenges to populator

--- src/main/java/com/thevirtualx/mvcApp/Populator.java
+++ src/main/java/com/thevirtualx/mvcApp/Populator.java
@@ -2,8 +2,10 @@ package com.thevirtualx.mvcApp;
 
 import com.thevirtualx.mvcApp.Entity.Account;
 import com.thevirtualx.mvcApp.Entity.Challenge;
-import com.thevirtualx.mvcApp.Entity.Comment;
-import com.thevirtualx.mvcApp.Storage.*;
+import com.thevirtualx.mvcApp.Storage.AccountStorage;
+import com.thevirtualx.mvcApp.Storage.ChallengeStorage;
+import com.thevirtualx.mvcApp.Storage.ChatroomStorage;
+import com.thevirtualx.mvcApp.Storage.GameStorage;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -33,15 +35,56 @@ public class Populator implements CommandLineRunner {
                 "https://4.bp.blogspot.com/-puG6A6cjmI8/VeDXT2if2bI/AAAAAAAAgnE/EIC6fuiO2NY/s1600/Blue%2BJay%252C%2B8-12-2015-3896-3.jpg", true, "USER");
         accountStorage.addAccount(connor);
 
-        Challenge mileWalk = new Challenge("Walk a mile durring lunch!", connor,
+        Account weiwei = new Account("iewiew", "GoWeiwei", "Weiwei Jiang", 100000,
+                "https://i.pinimg.com/originals/22/fd/a9/22fda9d0f11b8d86296d96b021062c90.jpg", true, "USER");
+        accountStorage.addAccount(weiwei);
+
+        Account bishnu = new Account("shnushnu", "GoWeiwei", "Bishnu Dahal", 1,
+                "https://i.pinimg.com/564x/9b/ed/cf/9bedcfb37de32251e932268d3141597a.jpg", true, "USER");
+        accountStorage.addAccount(bishnu);
+
+        Account branden = new Account("lynxBob", "GoWeiwei", "Branden Webb", 100,
+                "https://www.testbytes.net/wp-content/uploads/2019/06/Untitled-53-300x300.png", true, "USER");
+        accountStorage.addAccount(branden);
+
+        Account jon = new Account("jonnyboi", "GoWeiwei", "Jonathan Abrams", 100,
+                "https://media.zigcdn.com/media/content/2020/Mar/whatsappimage2020-03-28at12_39_27pm_560x420.jpg", true, "USER");
+        accountStorage.addAccount(jon);
+
+
+
+
+
+
+        Challenge mileWalk = new Challenge("Walk a mile during lunch!", connor,
                 "Eating is overrated, be a plant and soak up some sunshine!",
                 "https://www.thestatesman.com/wp-content/uploads/2017/12/WALKING00.jpg", 4, 10,
                 "Walk one mile every day this week.", "7 days", "everyone", true);
 
-        Comment mileCom1 = new Comment("I don't walk, I lift weights.");
-        mileWalk.addComment(mileCom1);
-        challengeStorage.addChallenge(mileWalk);
+//        Comment mileCom1 = new Comment("I don't walk, I lift weights.");
+//        mileWalk.addComment(mileCom1);
+//        challengeStorage.addChallenge(mileWalk);
 
+        Challenge flowerHunt = new Challenge("Lunch hour flower hunt", weiwei,
+                "Spring is finally here!!! Have you told yourself “Outside is so nice, I want to go outside today.” But as time goes by, you never make it outside.\n" +
+                        "This lunch hour flower hunt will give you a chance to go outside, and snip pictures of the flowers that you see in your yard, or on the way of your walk.\n" +
+                        "Enjoy the sun and get moving by doing this challenge.",
+                "https://www.thestatesman.com/wp-content/uploads/2017/12/WALKING00.jpg", 5, 10,
+                "Pick flowers every lunch break", "30 mins", "10", true);
+
+//        Comment gigitygigity = new Comment("oh right");
+//        flowerHunt.addComment(gigitygigity);
+//        challengeStorage.addChallenge(flowerHunt);
+
+        Challenge drinkWater = new Challenge("Water Bottle Challenge!", bishnu,
+                "Drink a bottle of water in under 1 minute!",
+                "https://i.ytimg.com/vi/OB2yQLOLVck/maxresdefault.jpg", 4, 10,
+                "Drink a bottle of water in 1 minute.", "1 min", "5", true);
+
+        Challenge floorIsLava = new Challenge("The Floor is Lava!", bishnu,
+                "Lava has breached the main floor! Jump from desk to desk to stay alive!",
+                "https://i.ytimg.com/vi/OB2yQLOLVck/maxresdefault.jpg", 4, 10,
+                "Floor is lava, Jumping", "30 min", "everyone", true);
 
 //        how do comments know where to go and who they belong to at the same time
     }
