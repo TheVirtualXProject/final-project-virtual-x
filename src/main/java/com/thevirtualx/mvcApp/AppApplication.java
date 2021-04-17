0afda69a8e1e7ae83991a27430af067ef4de8c76
Branden Webb <brandenswebb@hotmail.com>
2021-04-17 12:06:00 -0400

    added rating system

--- src/main/java/com/thevirtualx/mvcApp/AppApplication.java
+++ src/main/java/com/thevirtualx/mvcApp/AppApplication.java
@@ -1,5 +1,6 @@
 package com.thevirtualx.mvcApp;
 
+import com.thevirtualx.mvcApp.Entity.Rated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
