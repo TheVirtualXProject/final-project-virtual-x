c582f2d0ccc8bbf5414ce8477c40e76cf323a457
Branden Webb <brandenswebb@hotmail.com>
2021-04-26 18:32:31 -0400

    added war deployment support

--- src/main/java/com/thevirtualx/mvcApp/AppApplication.java
+++ src/main/java/com/thevirtualx/mvcApp/AppApplication.java
@@ -3,12 +3,20 @@ package com.thevirtualx.mvcApp;
 import com.thevirtualx.mvcApp.Entity.Rated;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppApplication {
+public class AppApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppApplication.class, args);
+	}
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AppApplication.class);
 	}
 
 	
